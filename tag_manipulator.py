1aedd4bc91360195434370cc818d90536d250376
pe78600 <pierre.ezard@gmail.com>
2021-07-29 16:46:52 +0100

    test 4 with 2 strings

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -1,8 +1,18 @@
+import re
+
 class TagManipulator():    
-    def parse_string(self, tags):
+    def parse_string(self, tags, regex=""):
         result = []
 
-        if len(tags) < 1 :
-            return result
+        tempResult = re.split( regex, tags )
+        
+        for i in range(0, len(tempResult)):
+
+
+            if( len(tempResult[i]) > 0 ):
+                item = tempResult[i]
+                item = re.sub('[\W_]+', '', item)
+                if len(item) > 0:
+                    result.append(tempResult[i].strip())  
 
         return result
--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -1,4 +1,5 @@
 from tag_manipulator import TagManipulator
+import re
 
 def test_split_empty_string_result_empty_array():
     # arrange
@@ -9,6 +10,47 @@ def test_split_empty_string_result_empty_array():
 
     # act
     result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+def test_split_comma_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+def test_split_one_string_result_array_of_one():
+    # arrange
+    stringToSplit = "java"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
+
+def test_split_two_string_result_array_of_one():
+    # arrange
+    stringToSplit = "java, python"
+    regex = ","
+    expResult = ["java", "python"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
 
     # assert
     assert result == expResult
